b15618cd3ee8f96ab7b30dfcbb9386e7e764b094
anjiboss <nhu.nhu.face@gmail.com>
2023-10-29 20:49:50 +0900

    decorate database instance

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,49 +1,51 @@
 import Elysia, { t } from "elysia";
-import { AppDataSource } from "../data-source";
 import { User } from "../entity/User";
+import { DataSource } from "typeorm";
 
-export const userController = new Elysia({ prefix: "/users" })
-    .get("/", () => {
-        const users = AppDataSource.manager.getRepository(User).find();
-        return users;
-    })
-    // .guard({
-    //     body: t.Object({
-    //         firstName: t.String(),
-    //         lastName: t.String(),
-    //         age: t.Number(),
-    //     }),
-    //     response: t.Object({
-    //         data: t.Object({
-    //             success: t.String(),
-    //         }),
-    //     }),
-    // })
-    .post(
-        "/",
-        (context) => {
-            const user = new User();
-            user.firstName = context.body.firstName;
-            user.lastName = context.body.lastName;
-            user.age = context.body.age;
-            AppDataSource.manager.save(user);
-            return {
-                data: {
-                    success: "ok",
-                    user,
-                },
-            };
-        },
-        {
-            body: t.Object({
-                firstName: t.String(),
-                lastName: t.String(),
-                age: t.Number(),
-            }),
-            response: t.Object({
-                data: t.Object({
-                    success: t.String(),
+export const userController = (db: DataSource) =>
+    new Elysia({ prefix: "/users" })
+        .decorate("db", db)
+        .get("/", ({ db }) => {
+            const users = db.manager.getRepository(User).find();
+            return users;
+        })
+        // .guard({
+        //     body: t.Object({
+        //         firstName: t.String(),
+        //         lastName: t.String(),
+        //         age: t.Number(),
+        //     }),
+        //     response: t.Object({
+        //         data: t.Object({
+        //             success: t.String(),
+        //         }),
+        //     }),
+        // })
+        .post(
+            "/",
+            (context) => {
+                const user = new User();
+                user.firstName = context.body.firstName;
+                user.lastName = context.body.lastName;
+                user.age = context.body.age;
+                context.db.manager.save(user);
+                return {
+                    data: {
+                        success: "ok",
+                        user,
+                    },
+                };
+            },
+            {
+                body: t.Object({
+                    firstName: t.String(),
+                    lastName: t.String(),
+                    age: t.Number(),
                 }),
-            }),
-        }
-    );
+                response: t.Object({
+                    data: t.Object({
+                        success: t.String(),
+                    }),
+                }),
+            }
+        );
--- server/src/index.ts
+++ server/src/index.ts
@@ -5,19 +5,19 @@ import cors from "@elysiajs/cors";
 import { logger } from "@grotto/logysia";
 import "reflect-metadata";
 import swagger from "@elysiajs/swagger";
-import { ServerWebSocket } from "bun";
 
 async function server() {
-    await AppDataSource.initialize();
+    const db = await AppDataSource.initialize();
 
     const app = new Elysia()
         .use(logger())
         .use(swagger())
         .use(cors())
-        .use(userController)
+        .use(userController(db))
         .ws("/ws", {
             body: t.Object({
                 type: t.String(),
+                data: t.Any(),
             }),
             message: (ws, message) => {
                 console.log(message);
