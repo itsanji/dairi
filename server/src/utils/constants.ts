db16b0c5d29aee43daac4528d6882509c85c1b19
its_anji <nhu.nhu.face@gmail.com>
2024-01-15 09:57:32 +0900

    auth for websocket connection

--- server/src/utils/constants.ts
+++ server/src/utils/constants.ts
@@ -1,5 +1,6 @@
 export const constants = {
     jwtSecret: Bun.env.JWT_SECRET || "super secret",
     jwtAccessExpire: Bun.env.JWT_ACCS_EXPIRE || "1d",
-    jwtRefreshExpire: Bun.env.JWT_REFR_EXPIRE || "30d"
+    jwtRefreshExpire: Bun.env.JWT_REFR_EXPIRE || "30d",
+    accessTokenKey: "access"
 };
