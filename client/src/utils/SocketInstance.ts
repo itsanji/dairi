e7f70d0f71c6a1f380a2c56408de759911d4d638
its_anji <nhu.nhu.face@gmail.com>
2024-01-19 07:45:49 +0900

    update socketInstance types and add open/close event setter

--- client/src/utils/SocketInstance.ts
+++ client/src/utils/SocketInstance.ts
@@ -1,21 +1,49 @@
-interface EventMapper {
-    [type: string]: (ev: any) => void;
+type SocketCallback<Data> = (ev: Data) => void;
+
+interface EventMapper<DataType> {
+    [roomId: string]: {
+        [type: string]: SocketCallback<DataType>;
+    };
 }
 
-export class SocketInstance {
+interface Options {
+    defaultEvents: {
+        open: (ev: Event) => void;
+        close: (ev: Event) => void;
+    };
+}
+const defaultOptions: Options = {
+    defaultEvents: {
+        open: () => {},
+        close: () => {}
+    }
+};
+
+export class SocketInstance<SocketDataType> {
     socket: WebSocket;
-    eventMapper: EventMapper;
-    constructor(socket: WebSocket) {
+    eventMapper: EventMapper<any>;
+    readonly GLOBAL = "global";
+    roomId: string = this.GLOBAL;
+
+    constructor(socket: WebSocket, options: Options = defaultOptions) {
         this.socket = socket;
 
         // adding default behaviour
         this.eventMapper = {
-            open: () => {},
-            close: () => {},
-            error: () => {}
+            // default room
+            [this.GLOBAL]: {
+                _: () => {}
+            }
         };
-        this.socket.onopen = () => {
-            console.log("socket opened");
+        this.on<Event>("open", options.defaultEvents.open);
+        this.on<Event>("close", options.defaultEvents.close);
+
+        this.socket.onopen = (ev) => {
+            this.eventMapper[this.GLOBAL].open(ev);
+        };
+
+        this.socket.onclose = (ev) => {
+            this.eventMapper[this.GLOBAL].close(ev);
         };
     }
 
@@ -28,20 +56,39 @@ export class SocketInstance {
         );
     }
 
-    on<T = SocketData>(type: string, callback: (ev: T) => void) {
-        if (!this.eventMapper[type]) {
-            this.eventMapper[type] = callback;
+    /**
+     * Add Event Listner to current room. if room not set, event will registered to GLOBAL room
+     * @param type event name
+     * @param callback function will be fired when receive event
+     * @example
+     * ```typescript
+     *  socket.on("boo", (data) => { console.log(data.msg) })
+     *  // This function is generic. default type of event callback
+     *  is type passed to SocketWrapper.
+     *  socket.on<{foo: string, bar: number}>("boo", (data) => {
+     *    console.log(data.foo, data.bar + 1)
+     *  })
+     * ```
+     */
+    on<T = SocketDataType>(type: string, callback: SocketCallback<T>) {
+        if (!this.eventMapper[this.roomId][type]) {
+            // setting new event into current room
+            this.eventMapper[this.roomId][type] = callback;
         }
+
         this.socket.onmessage = (event) => {
-            console.log(event, this.eventMapper);
+            console.log({ event, eventMapper: this.eventMapper, room: this.roomId });
+
             let data: MessageEvent<T>;
             try {
                 data = JSON.parse(event.data);
-                const cb = this.eventMapper[data.type];
-                cb(data.data);
+                const cb = this.eventMapper[this.roomId][data.type];
+                cb ? cb(data.data) : () => {};
             } catch (e) {
                 console.log("system error: cannot parse server response");
             }
         };
+
+        return this;
     }
 }
