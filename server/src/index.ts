5c7b1e63bcb6ca3e15dd0a5c50f7cbadc6754bca
itsanji <nhu.nhu.face@gmail.com>
2024-10-28 22:51:16 +0900

    add redis and cron job

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,11 +6,13 @@ import { authController } from "./controllers/auth.controller";
 import { userController } from "./controllers/user.controller";
 import { wsController } from "./controllers/ws.controller";
 import { logger } from "./utils/log";
+import { jobEntry } from "./jobs/index.job";
 
 const app = new Elysia()
     .use(logger())
     .use(swagger())
     .use(cors())
+    .use(jobEntry)
     .get("/health", () => ({ message: "application feeling good" }))
     // below is private routes
     .use(authController)
